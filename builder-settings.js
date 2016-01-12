4b02d50b84f733094ace243bddd416917a8c2353
Paula Lavalle <lavalle.paula@gmail.com>
2016-01-11 16:12:34 -0800

    Add builder fonts and colors.

---
+++ builder-settings.js
@@ -0,0 +1,29 @@
+/* eslint-disable max-len */
+module.exports = {
+  // --------
+  // Fonts
+  // --------
+  sansSerif: "'Acumin Pro', 'Karla', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+  monospace: "'Source Code Pro', 'Inconsolata', 'Courier New', 'Courier', monospace",
+  // --------
+  // Colors
+  // --------
+  // Blacks
+  darkerJet: "#0a0a0a",
+  darkJet: "#111111",
+  jet: "#151515",
+  lightJet: "#1f1f1f",
+  // Grays
+  darkGray: "#878787",
+  gray: "#d7d7d7",
+  white: "#fafafa",
+  // Accent
+  gold: "#fff982",
+  darkGold: "#ffea00",
+  // Formidable Reds
+  palestRed: "#E5847D",
+  palerRed: "#C75B54",
+  paleRed: "#bd4139",
+  red: "#bd1e13"
+};
+/* eslint-enable max-len */
