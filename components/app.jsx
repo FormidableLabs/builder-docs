6efd6bc7f800132c524932b9cb738d05e17c669b
Paula Lavalle <lavalle.paula@gmail.com>
2016-01-13 13:49:44 -0800

    WIP Add builder intro copy

--- components/app.jsx
+++ components/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Radium, { Style } from "radium";
 
+import Diagram from "./diagram-flavors";
 import Docs from "./docs";
 import Hero from "./hero";
 import { Header, Footer } from "formidable-landers";
@@ -28,7 +29,7 @@ class App extends React.Component {
       zIndex: "0",
       position: "relative",
       margin: "0",
-      padding: "0",
+      padding: "0 0 2em 0",
       width: "100%",
       background: settings.white
     };
@@ -41,13 +42,27 @@ class App extends React.Component {
         <Hero />
         <main style={this.getMainStyles()}>
           <div style={this.getBackgroundSkewStyles()}></div>
-          <div>
-            <h3>Simple multiproject management through composable npm task workflows.</h3>
-            <p>
-              npm is great for controlling a single repository’s dependencies and tasks. But what if you need <strong>shared infrastructure and configuration across multiple repos</strong>?
-            </p>
-          </div>
           <div className="Container">
+            <h3 className="Headline">
+              Simple multiproject management through composable npm task workflows.
+            </h3>
+            <div className="Focus">
+              <p>
+                <code>npm</code> is great for controlling a single repository’s dependencies and tasks. But what if you need <strong>shared infrastructure and configuration across multiple repos</strong>?
+              </p>
+            </div>
+            <Diagram />
+            <div className="Focus">
+              <p>
+                Builder is a meta tool for controlling npm workflows. If you’re working on multiple similar repos, it allows you to <strong>define tasks and dependencies in a single shared source of truth</strong>.
+              </p>
+            </div>
+            <Diagram />
+            <div className="Focus">
+              <p>
+                Or, if you’re working in a single repo, it allows you to dramatically simplify your <code>package.json</code>. Either way, it keeps you close to vanilla npm—you don’t need to buy into a whole new build philosophy to start benefiting from builder today.
+              </p>
+            </div>
             <div className="Copy">
               <Docs />
             </div>
